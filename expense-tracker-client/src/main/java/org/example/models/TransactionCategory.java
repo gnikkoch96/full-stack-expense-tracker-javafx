1f9bd55f664176351cc23fd4b7a0d64d545e0432
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2024-11-27 19:14:46 -0800

    add proper fields, constructor, and getters/setters to transaction category model in client

--- expense-tracker-client/src/main/java/org/example/models/TransactionCategory.java
+++ expense-tracker-client/src/main/java/org/example/models/TransactionCategory.java
@@ -1,4 +1,39 @@
 package org.example.models;
 
 public class TransactionCategory {
+    private int id;
+    private int userId;
+    private String categoryName;
+    private String categoryColor;
+
+    public TransactionCategory(int id, int userId, String categoryName, String categoryColor) {
+        this.id = id;
+        this.userId = userId;
+        this.categoryName = categoryName;
+        this.categoryColor = categoryColor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryColor() {
+        return categoryColor;
+    }
+
+    public void setCategoryColor(String categoryColor) {
+        this.categoryColor = categoryColor;
+    }
 }
