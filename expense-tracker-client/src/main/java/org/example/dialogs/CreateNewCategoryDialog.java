f556f176bcbde38135c182facc5ff811b172acc4
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2024-11-29 19:23:41 -0800

    create view and edit category dialog

--- expense-tracker-client/src/main/java/org/example/dialogs/CreateNewCategoryDialog.java
+++ expense-tracker-client/src/main/java/org/example/dialogs/CreateNewCategoryDialog.java
@@ -1,6 +1,5 @@
 package org.example.dialogs;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -17,10 +16,14 @@ import org.example.utils.ApiHandler;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 
+// Note: add validation
 // Note: the way the dialog works is that it expects to return something but that is not necessary for us
 public class CreateNewCategoryDialog extends Dialog<String> {
     private User user;
     private Alert infoAlert, errorAlert;
+    private TextField newCategoryTextField;
+    private ColorPicker colorPicker;
+    private Button createCategoryButton;
 
     public CreateNewCategoryDialog(User user){
         this.user = user;
@@ -49,37 +52,23 @@ public class CreateNewCategoryDialog extends Dialog<String> {
         VBox dialogVBox = new VBox(20);
         dialogVBox.setAlignment(Pos.CENTER);
 
-        TextField newCategoryTextField = new TextField();
+        newCategoryTextField = new TextField();
         newCategoryTextField.setPromptText("Create new Category");
         newCategoryTextField.setFocusTraversable(false);
         newCategoryTextField.getStyleClass().addAll("text-size-md", "field-background", "text-light-gray");
 
-        ColorPicker colorPicker = new ColorPicker();
+        colorPicker = new ColorPicker();
         colorPicker.getStyleClass().addAll("text-size-md");
         colorPicker.setMaxWidth(Double.MAX_VALUE);
 
-        Button createCategoryButton = new Button("Create");
+        createCategoryButton = new Button("Create");
         createCategoryButton.getStyleClass().addAll("text-size-md", "bg-light-blue", "text-white");
         createCategoryButton.setMaxWidth(Double.MAX_VALUE);
         createCategoryButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 // retrieve the values
-                String categoryName = newCategoryTextField.getText();
-                String color = colorPicker.getValue().toString();
-
-                // Note: the reason why we removed two is because ColorPicker returns a hex that includes the alpha values which
-                // we don't want. They are the last two characters so we substring them out before storing them into the database.
-                String hexColorValue = color.substring(0, color.length() - 2);
-
-                JsonObject transactionCategoryData = new JsonObject();
-
-                JsonObject userData = new JsonObject();
-                userData.addProperty("id", user.getId());
-
-                transactionCategoryData.add("user", userData);
-                transactionCategoryData.addProperty("categoryName", categoryName);
-                transactionCategoryData.addProperty("categoryColor", hexColorValue);
+                JsonObject transactionCategoryData = createCategoryJsonData();
 
                 HttpURLConnection httpConn = null;
                 try{
@@ -108,5 +97,24 @@ public class CreateNewCategoryDialog extends Dialog<String> {
 
         dialogVBox.getChildren().addAll(newCategoryTextField, colorPicker, createCategoryButton);
         return dialogVBox;
+    }
+
+    private JsonObject createCategoryJsonData() {
+        String categoryName = newCategoryTextField.getText();
+        String color = colorPicker.getValue().toString();
+
+        // Note: the reason why we removed two is because ColorPicker returns a hex that includes the alpha values which
+        // we don't want. They are the last two characters so we substring them out before storing them into the database.
+        String hexColorValue = color.substring(0, color.length() - 2);
+
+        JsonObject transactionCategoryData = new JsonObject();
+
+        JsonObject userData = new JsonObject();
+        userData.addProperty("id", user.getId());
+
+        transactionCategoryData.add("user", userData);
+        transactionCategoryData.addProperty("categoryName", categoryName);
+        transactionCategoryData.addProperty("categoryColor", hexColorValue);
+        return transactionCategoryData;
     }
 }
