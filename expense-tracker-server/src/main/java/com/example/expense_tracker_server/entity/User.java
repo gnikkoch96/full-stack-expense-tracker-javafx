55bfe5f61fd39769d8009124dc2bf7f2342e446c
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2024-12-04 08:50:59 -0800

    update user entity to include createdAt property

--- expense-tracker-server/src/main/java/com/example/expense_tracker_server/entity/User.java
+++ expense-tracker-server/src/main/java/com/example/expense_tracker_server/entity/User.java
@@ -2,6 +2,8 @@ package com.example.expense_tracker_server.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity // marks class as JPA entity which means that it will be mapped to the user table in our db
 @Table(name = "user") // labels this entity to the proper name which is "users"
 public class User {
@@ -17,6 +19,9 @@ public class User {
 
     @Column(name="password")
     private String password;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
 
     public int getId() {
         return id;
@@ -44,5 +49,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
