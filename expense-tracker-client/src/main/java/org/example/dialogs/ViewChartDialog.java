f36ff45be43f40e2e89abdfcaa916f2217069977
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-01-13 20:17:05 -0800

    add: bar color changed to match appropriate colors for income and expense

--- expense-tracker-client/src/main/java/org/example/dialogs/ViewChartDialog.java
+++ expense-tracker-client/src/main/java/org/example/dialogs/ViewChartDialog.java
@@ -1,24 +1,65 @@
 package org.example.dialogs;
 
 import javafx.collections.ObservableList;
-import javafx.scene.chart.PieChart;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
 import org.example.models.MonthlyFinance;
 import org.example.models.User;
 
 public class ViewChartDialog extends CustomDialog{
-    private ObservableList<MonthlyFinance> monthlyFinances; // will store data for the correspnoding year
-    private PieChart pieChart;
+    private ObservableList<MonthlyFinance> monthlyFinances;
+    private BarChart<String, Number> barChart;
 
     public ViewChartDialog(User user, ObservableList<MonthlyFinance> monthlyFinances) {
         super(user);
         this.monthlyFinances = monthlyFinances;
-        pieChart = new PieChart();
+        setTitle("View Chart");
+        setWidth(700);
+        setHeight(595);
+
+        // needed to add a vbox for the chart to show
+        VBox vbox = new VBox();
+
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Month");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Value");
+
+        barChart = new BarChart<>(xAxis, yAxis);
+        barChart.setMinWidth(getWidth() - 25);
+        barChart.setMinHeight(getHeight() - 50);
+        barChart.getStyleClass().addAll("text-size-md");
+
+        XYChart.Series<String, Number> incomeSeries = new XYChart.Series<>();
+        incomeSeries.setName("income");
+
+        XYChart.Series<String, Number> expenseSeries = new XYChart.Series<>();
+        expenseSeries.setName("expense");
 
         for(MonthlyFinance monthlyFinance : monthlyFinances){
-            PieChart.Data slice = new PieChart.Data(monthlyFinance.getMonth(), monthlyFinance.getIncome().doubleValue());
-            pieChart.getData().add(slice);
+            incomeSeries.getData().add(new XYChart.Data<>(monthlyFinance.getMonth(), monthlyFinance.getIncome()));
+            expenseSeries.getData().add(new XYChart.Data<>(monthlyFinance.getMonth(), monthlyFinance.getExpense()));
         }
 
-        getDialogPane().getChildren().add(pieChart);
+        barChart.getData().addAll(incomeSeries, expenseSeries);
+
+
+        // update the colors for income
+        incomeSeries.getData().forEach(data ->
+                data.getNode().setStyle("-fx-bar-fill: green;")
+        );
+
+        // update the colors for expense
+        expenseSeries.getData().forEach(data ->
+                data.getNode().setStyle("-fx-bar-fill: red;")
+        );
+
+
+        vbox.getChildren().add(barChart);
+        getDialogPane().getChildren().add(vbox);
     }
 }
